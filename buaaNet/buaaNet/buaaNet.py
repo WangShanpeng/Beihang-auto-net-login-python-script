60afe6df49a1e6ade831ae6a03d0882bbfae87f2
xienaiwen <sienaiwun@gmail.com>
2016-12-26 22:56:12 +0800

    添加批处理文件

--- buaaNet/buaaNet/buaaNet.py
+++ buaaNet/buaaNet/buaaNet.py
@@ -1,21 +1,33 @@
-import urllib
+﻿import urllib
 import urllib2
 import base64
+import time
 class login:
     def __init__(self,Username,Password):
-        self.Url = 'https://gw.buaa.edu.cn:803/include/auth_action.php'
-        Password=base64.b64encode(Password)
-        L=list(Password)
-        for a in range(len(L)):
-           if L[a]=='=':
-               L[a]='%3D'
-        Password=''.join(L)
-        self.Data = 'action=login&username='+Username+'&password={B}'+Password+'&ac_id=4&user_ip=&nas_ip=&user_mac=&save_me=1&ajax=1'
+        self.Url = 'https://gw.buaa.edu.cn:801/include/auth_action.php'
+        Password = base64.b64encode(Password)
+        Password = urllib.quote(Password)
+        self.Data = 'action=login&username='+Username+'&password={B}'+Password+'&ac_id=22&user_ip=&nas_ip=&user_mac=&save_me=1&ajax=1'
     def auto_login(self):
         request = urllib2.Request(url=self.Url,data=self.Data)
         response = urllib2.urlopen(request)
         print response.read()
+        
+#####################################################
+def internet_on():
+    try:
+        urllib2.urlopen('https://www.baidu.com/', timeout=1)
+        return True
+    except urllib2.URLError as err: 
+        return False
 
 #####################################################
-Buaa = login(Username='by1206148',Password='19880207')
-Buaa.auto_login()
+if __name__ == '__main__':
+    while True:
+        if not internet_on():
+            Buaa = login(Username='by1206148',Password='19880207')
+            Buaa.auto_login()
+            print "user login"
+        time.sleep(60*2)
+  
+
